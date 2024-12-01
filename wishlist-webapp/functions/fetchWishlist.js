9e66eac5086eb76caf396df019c76f7d59ab4232
Sebastian Givskud <95365210+sgivskud@users.noreply.github.com>
2024-12-01 20:32:47 +0100

    CORS update fetchWishlist.js

--- wishlist-webapp/functions/fetchWishlist.js
+++ wishlist-webapp/functions/fetchWishlist.js
@@ -1,8 +1,25 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    // Replace 'https://www.yourshopifydomain.com' with your actual domain
+    'Access-Control-Allow-Origin': 'https://www.brunodesign.dk',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    // Handle CORS preflight request
+    return {
+      statusCode: 200,
+      headers,
+      body: '',
+    };
+  }
+
   if (event.httpMethod !== 'GET') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return { statusCode: 405, headers, body: 'Method Not Allowed' };
   }
 
   const customerId = event.queryStringParameters.customerId;
@@ -10,13 +27,14 @@ exports.handler = async (event, context) => {
   if (!customerId) {
     return {
       statusCode: 400,
+      headers,
       body: JSON.stringify({ success: false, message: 'Missing customerId.' }),
     };
   }
 
   try {
     const adminApiUrl = `https://${process.env.SHOPIFY_SHOP_DOMAIN}/admin/api/2023-10/graphql.json`;
-    const headers = {
+    const apiHeaders = {
       'Content-Type': 'application/json',
       'X-Shopify-Access-Token': process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN,
     };
@@ -34,25 +52,27 @@ exports.handler = async (event, context) => {
 
     const response = await fetch(adminApiUrl, {
       method: 'POST',
-      headers,
+      headers: apiHeaders,
       body: JSON.stringify({ query }),
     });
 
     const result = await response.json();
     let wishlist = [];
 
-    if (result.data.customer.metafield) {
+    if (result.data.customer && result.data.customer.metafield) {
       wishlist = JSON.parse(result.data.customer.metafield.value);
     }
 
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify({ success: true, wishlist }),
     };
   } catch (error) {
     console.error('Error fetching wishlist:', error);
     return {
       statusCode: 500,
+      headers,
       body: JSON.stringify({ success: false, error: error.message }),
     };
   }
